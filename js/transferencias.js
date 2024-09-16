1364a080c8c9b68c11440572c18ae02fb0f98e59
martinazanettap <zanettamarti@gmail.com>
2024-09-16 10:04:37 -0300

    cambios
    cambios

--- js/transferencias.js
+++ js/transferencias.js
@@ -4,49 +4,37 @@ formulario.addEventListener('submit', function (event) {
 	const usuarioActual = localStorage.getItem('usuarioActual');
 	const usuarios = JSON.parse(localStorage.getItem('usuarios'));
 
-	const nombreUsuario = document.getElementById('nombre-usuario').value;
-	const monto = parseFloat(document.getElementById('monto').value);
-	const motivo = document.getElementById('motivo').value;
+// validamos campos
+if (numeroFactura === '' || monto === '' || metodoPago === '') {
+	mostrarError('Todos los campos son obligatorios');
+	return;
+}
 
-	// validamos campos
-	if (nombreUsuario === '' || monto === '' || motivo === '') {
-		mostrarError('Todos los campos son obligatorios');
-		return;
-	}
+if (monto <= 0) {
+	mostrarError('El monto mínimo debe ser mayor a 0');
+	return;
+}
 
-	if (monto <= 0) {
-		mostrarError('El monto mínimo debe ser mayor a 0');
-		return;
-	}
 
 	// validamos si el usuario tiene suficiente saldo para realizar el pago
-	if (usuarios[usuarioActual.toLowerCase()].saldo >= monto) {
+
+	// validamos si el usuario tiene suficiente saldo para realizar el pago
+	if (usuarios[usuarioActual.toLocaleLowerCase()].saldo >= monto) {
 		// actualizamos el saldo del usuario
-		if (usuarios[nombreUsuario]) {
-			usuarios[nombreUsuario.toLowerCase()].saldo += monto;
-			usuarios[nombreUsuario.toLowerCase()].historialTransferencias.push({
-				monto: monto,
-				motivo: motivo,
-				emisor: usuarioActual,
-				receptor: usuarios[nombreUsuario.toLocaleLowerCase()].nombre,
-				esIngreso: true,
-				fecha: new Date().toISOString().slice(0, 10)
-			});
-		}
 		usuarios[usuarioActual.toLocaleLowerCase()].saldo -= monto;
+
 		// registramos el pago en el historial de pagos
-		usuarios[usuarioActual.toLocaleLowerCase()].historialTransferencias.push({
+		usuarios[usuarioActual.toLocaleLowerCase()].historialPagos.push({
+			numero: numeroFactura,
 			monto: monto,
-			motivo: motivo,
-			emisor: usuarioActual,
-			receptor: nombreUsuario,
-			esIngreso: false,
+			metodo: metodoPago,
 			fecha: new Date().toISOString().slice(0, 10) // Formato YYYY-MM-DD
 		});
+
 		actualizarUsuariosEnLocalStorage(usuarios);
-		mostrarExito('Transferencia realizada con éxito');
+		mostrarExito('Pago realizado con éxito');
 	} else {
-		mostrarError('Saldo insuficiente para realizar la transferencia.');
+		mostrarError('Saldo insuficiente para realizar el pago.');
 	}
 });
 
